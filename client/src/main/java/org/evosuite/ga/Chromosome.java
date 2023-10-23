52d5a5c529d68fbcb168b317c9b4f211b1b9580e
João Afonso <johnlandon1999@gmail.com>
2023-10-23 19:12:40 +0100

    Requested correction in Chromosome imports

--- client/src/main/java/org/evosuite/ga/Chromosome.java
+++ client/src/main/java/org/evosuite/ga/Chromosome.java
@@ -26,7 +26,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.averagingDouble;
 
